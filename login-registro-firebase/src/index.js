592fa350fdd4b3977bf7e2e47a2293c88460200f
yudithkleon <leonyudithk@gmail.com>
2022-02-25 09:18:16 -0500

    maquetacion de login registro y routers

---
+++ login-registro-firebase/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppRouters from './routers/AppRouters';
+
+import './styles/styles.css'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <AppRouters/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
